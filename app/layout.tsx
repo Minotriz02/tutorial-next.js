3da5d67635b1b9ff36a2a59f4d7d3f0b96de7216
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-01-10 14:40:14 -0500

    next.js tutorial finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { monstserrat } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${monstserrat.className} antialiased`}>
+        {children}
+        <footer className="flex items-center justify-center py-10">
+          Hecho con amor por Vercel
+        </footer>
+      </body>
     </html>
   );
 }
